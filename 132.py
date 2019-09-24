9bd188acebee42c1fd6d0fd9dfd2027eec07ae83
dimAss <dima.gavrilov-shlyapa@mail.ru>
2019-09-24 10:59:15 +0300

    132

--- 132.py
+++ 132.py
@@ -1,6 +1,7 @@
 from graphics import *
 win=GraphWin("MyWin",1000,1000)
 
+c=Circle(Point(500,500),40)
 r=Rectangle(Point(250,500),Point(750,900))
 r.setFill("#B97A57")
 
@@ -13,8 +14,8 @@ p.setFill("#ED1C24")
 r2=Rectangle(Point(275,600),Point(450,900))
 r2.setFill("#482425")
 
-c=Circle(Point(425,750),10)
-c.setFill("#FFF200")
+c1=Circle(Point(425,750),10)
+c1.setFill("#FFF200")
 
 r3=Rectangle(Point(520,620),Point(720,820))
 r3.setFill("#99D9EA")
@@ -23,21 +24,12 @@ l=Line(Point(620,620),Point(620,820))
 
 l1=Line(Point(620,660),Point(720,660))
 
-r4=Rectangle(Point(0,850),Point(1000,1000))
-r4.setFill("green")
 
-c1=Circle(Point(900,100),50)
-c1.setFill("yellow")
-
-
-
-r4.draw(win)
-c1.draw(win)
 r.draw(win)
 r1.draw(win)
 p.draw(win)
 r2.draw(win)
-c.draw(win)
+c1.draw(win)
 r3.draw(win)
 l.draw(win)
 l1.draw(win)
